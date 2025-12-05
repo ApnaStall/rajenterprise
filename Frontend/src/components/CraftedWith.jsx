c6abb2d3b19972f90a2a06d67722303ce9d6ea84
Dax <apnastall.project@gmail.com>
2025-12-05 11:24:25 +0530

    CraftedWith Button

--- Frontend/src/components/CraftedWith.jsx
+++ Frontend/src/components/CraftedWith.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function CraftedWith() {
+
+  const navigate = useNavigate();
+
   return (
     <>
-    <div>
-      <section className="w-full py-10 px-6 md:px-20">
-                <div className="items-center mx-auto text-start">
-                  
-                  <h2 className="text-3xl md:text-4xl font-semibold text-[#003049] mb-4">
-                    Crafted with excellent material.
-                  </h2>
-        
-                  <p className="text-gray-700 leading-relaxed text-lg">
-                    "At <span className="font-bold">ROSE GARMENT</span>, we specialize in crafting hospital linen clothes 
-                    with exceptional attention to quality and durability. Each garment is meticulously crafted 
-                    using premium materials to ensure comfort, longevity, and hygienic performance in healthcare 
-                    environments. Our commitment to excellence extends to every stitch, providing healthcare 
-                    professionals with attire they can trust."
-                  </p>
-        
-                  <button className="mt-6 bg-[#003049] text-[#f2a900] font-semibold py-3 px-8 rounded-xl shadow-md hover:bg-[#000000] hover:text-white transition">
-                    Explore
-                  </button>
-                </div>
-              </section>
-    </div>
+      <div>
+        <section className="w-full py-10 px-6 md:px-20">
+          <div className="items-center mx-auto text-start">
+            
+            <h2 className="text-3xl md:text-4xl font-semibold text-[#003049] mb-4">
+              Crafted with excellent material.
+            </h2>
+
+            <p className="text-gray-700 leading-relaxed text-lg">
+              "At <span className="font-bold">ROSE GARMENT</span>, we specialize in crafting hospital linen clothes 
+              with exceptional attention to quality and durability. Each garment is meticulously crafted 
+              using premium materials to ensure comfort, longevity, and hygienic performance in healthcare 
+              environments. Our commitment to excellence extends to every stitch, providing healthcare 
+              professionals with attire they can trust."
+            </p>
+
+            <button
+              onClick={() => navigate("/Shop")}
+              className="mt-6 bg-[#003049] text-[#f2a900] font-semibold py-3 px-8 rounded-xl shadow-md hover:bg-[#000000] hover:text-white transition"
+            >
+              Explore
+            </button>
+
+          </div>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default CraftedWith
+export default CraftedWith;
